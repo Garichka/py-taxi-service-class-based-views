5a67450efa29092e63fafa892a43501a944e7539
artem <artem90078@gmail.com>
2026-03-30 11:08:30 +0300

    Solution

--- taxi/views.py
+++ taxi/views.py
@@ -4,40 +4,40 @@ from taxi.models import Driver, Car, Manufacturer
 
 
 class ManufacturerListView(generic.ListView):
-  model = Manufacturer
-  queryset = Manufacturer.objects.all().order_by("name")
-  paginate_by = 5
+    model = Manufacturer
+    queryset = Manufacturer.objects.all().order_by("name")
+    paginate_by = 5
 
 
 class CarListView(generic.ListView):
-  model = Car
-  queryset = Car.objects.select_related("manufacturer")
-  paginate_by = 5
+    model = Car
+    queryset = Car.objects.select_related("manufacturer")
+    paginate_by = 5
 
 
 class CarDetailView(generic.DetailView):
-  model = Car
+    model = Car
 
 
 class DriverListView(generic.ListView):
-  model = Driver
-  paginate_by = 5
+    model = Driver
+    paginate_by = 5
 
 
 class DriverDetailView(generic.DetailView):
-  model = Driver
-  queryset = Driver.objects.prefetch_related("cars__manufacturer")
+    model = Driver
+    queryset = Driver.objects.prefetch_related("cars__manufacturer")
 
 
 def index(request):
-  num_drivers = Driver.objects.count()
-  num_cars = Car.objects.count()
-  num_manufacturers = Manufacturer.objects.count()
+    num_drivers = Driver.objects.count()
+    num_cars = Car.objects.count()
+    num_manufacturers = Manufacturer.objects.count()
 
-  context = {
-    "num_drivers": num_drivers,
-    "num_cars": num_cars,
-    "num_manufacturers": num_manufacturers,
-  }
+    context = {
+        "num_drivers": num_drivers,
+        "num_cars": num_cars,
+        "num_manufacturers": num_manufacturers,
+    }
 
-  return render(request, "taxi/index.html", context=context)
+    return render(request, "taxi/index.html", context=context)
